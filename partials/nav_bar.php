9f28f4142b22977ecdd7654ef9627fd18e104e15
Doris Liu <chiayuu.liu@gmail.com>
2021-08-18 19:49:57 +0800

    更新navbar for 串接用

--- partials/nav_bar.php
+++ partials/nav_bar.php
@@ -1,25 +1,34 @@
+<style>
+    li a{
+        font-weight: bold;
+    }
+    form a{
+        font-weight: bold;
+    }
+</style>
+
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="product_list.php">商品列表</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="?cate=1">快速上桌 </a>
+                    <a class="nav-link" href="product_list.php">商品列表 </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="?cate=2">健身專區</a>
+                    <a class="nav-link" href="">文章專欄 </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">餐廳資訊</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">購物車</a>
                 </li>
                 
-                <li class="nav-item" name="material" >
-                    <a class="nav-link" onclick="" href="?cate=3" >嚴選食材</a>
-                </li>
-
             </ul>
                 <form class="form-inline my-2 my-lg-0">
-                    <a class="nav-link" href="product_create.php">新增商品 </a>
+                    <a class="nav-link" href="">登入 </a>
+                    <a class="nav-link" href="">註冊 </a>                   
                 </form>
         </div>
     </div>
